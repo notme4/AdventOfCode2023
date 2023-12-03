e347bf0ce424b69218e05e1c40347e23f811a350
notme4 <102337330+notme4@users.noreply.github.com>
2023-12-03 03:47:14 -0600

    create part1 file

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,7 @@
+pub mod part1;
+
+use crate::part1::trebuchet;
+
 fn main() {
-    println!("Hello, world!");
+    trebuchet();
 }
---
+++ day1/src/part1.rs
@@ -0,0 +1,3 @@
+pub fn trebuchet() {
+    println!("trebuchet");
+}
