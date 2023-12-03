8e421dca5fae8fde578af2202280a1c7dfb805e2
notme4 <102337330+notme4@users.noreply.github.com>
2023-12-03 13:02:26 -0600

    finish part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,12 @@
-pub mod part1;
+pub mod part2;
 
-use crate::part1::trebuchet;
+use crate::part2::trebuchet;
 
 use std::path::Path;
 
 fn main() {
     // let path = Path::new("resources/exampleData.txt");
+    // let path = Path::new("resources/exampleData2.txt");
     let path = Path::new("resources/myData.txt");
 
     println!("{}", trebuchet(&path));
--- day1/src/part2.rs
+++ day1/src/part2.rs
@@ -1,21 +1,58 @@
 use std::fs::read_to_string;
 use std::path::Path;
 
-fn first_last_number(s: &str) -> u32 {
-    let mut first = u32::max_value();
-    let mut last = u32::max_value();
-    for c in s.chars() {
-        if c.is_numeric() {
-            if first == u32::max_value() {
-                first = c.to_digit(10).unwrap();
+fn first_last_number(line: &str) -> usize {
+    let mut first = usize::MAX;
+    let mut first_pos = usize::MAX;
+    let mut last = usize::MAX;
+    let mut last_pos = 0;
+
+    let search_strs = [
+        ("one", 1),
+        ("1", 1),
+        ("two", 2),
+        ("2", 2),
+        ("three", 3),
+        ("3", 3),
+        ("four", 4),
+        ("4", 4),
+        ("five", 5),
+        ("5", 5),
+        ("six", 6),
+        ("6", 6),
+        ("seven", 7),
+        ("7", 7),
+        ("eight", 8),
+        ("8", 8),
+        ("nine", 9),
+        ("9", 9),
+        ("0", 0),
+    ];
+    for p in search_strs {
+        let mut i = line.find(p.0);
+        first = if i.is_some_and(|x| x < first_pos) {
+            unsafe {
+                first_pos = i.unwrap_unchecked();
             }
-            last = c.to_digit(10).unwrap();
-        }
+            p.1
+        } else {
+            first
+        };
+
+        i = line.rfind(p.0);
+        last = if i.is_some_and(|x| x >= last_pos) {
+            unsafe {
+                last_pos = i.unwrap_unchecked();
+            }
+            p.1
+        } else {
+            last
+        };
     }
     return 10 * first + last;
 }
 
-pub fn trebuchet(path: &Path) -> u32 {
+pub fn trebuchet(path: &Path) -> usize {
     return read_to_string(path)
         .unwrap()
         .lines()
