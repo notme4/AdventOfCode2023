81329b809af9f1b8680d0ef9c448e9eba85350f8
notme4 <102337330+notme4@users.noreply.github.com>
2023-12-03 04:45:22 -0600

    finished part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,18 +2,11 @@ pub mod part1;
 
 use crate::part1::trebuchet;
 
-use std::fs::File;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("resources/exampleData.txt");
-    // let path = Path::new("../resources/myData.txt");
-    let display = path.display();
+    // let path = Path::new("resources/exampleData.txt");
+    let path = Path::new("resources/myData.txt");
 
-    let file = match File::open(&path) {
-        Err(why) => panic!("Couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    trebuchet(file);
+    println!("{}", trebuchet(&path));
 }
--- day1/src/part1.rs
+++ day1/src/part1.rs
@@ -1,6 +1,24 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::read_to_string;
+use std::path::Path;
 
-pub fn trebuchet(file: File) {
-    println!("trebuchet");
+fn first_last_number(s: &str) -> u32 {
+    let mut first = u32::max_value();
+    let mut last = u32::max_value();
+    for c in s.chars() {
+        if c.is_numeric() {
+            if first == u32::max_value() {
+                first = c.to_digit(10).unwrap();
+            }
+            last = c.to_digit(10).unwrap();
+        }
+    }
+    return 10 * first + last;
+}
+
+pub fn trebuchet(path: &Path) -> u32 {
+    return read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(first_last_number)
+        .sum();
 }
