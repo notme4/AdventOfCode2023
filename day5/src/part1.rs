87dac028f3ab3eced63ede1d4e0c4085b00c0361
notme4 <102337330+notme4@users.noreply.github.com>
2023-12-29 17:29:48 -0600

    get_seed_numbers()

--- day5/src/part1.rs
+++ day5/src/part1.rs
@@ -1,9 +1,29 @@
 use std::fs::read_to_string;
 use std::path::Path;
 
+type Mapping = (u32, u32, u32); // start from, start to, len
+
+fn in_mapping(id: u32, mapping: Mapping) -> bool {
+    return mapping.0 < id && id < mapping.0 + mapping.2;
+}
+
+fn to_vec_u32(line: &str) -> Vec<u32> {
+    line.split(" ")
+        .filter_map(|val| val.parse::<u32>().ok())
+        .collect()
+}
+
+fn get_seed_numbers(seed_str: &str) -> Vec<(u32, u32)> {
+    let (_, seed_str) = seed_str.split_at(seed_str.find("\n").unwrap() + 1);
+    return to_vec_u32(seed_str).into_iter().map(|val| (val, val)).collect();
+}
+
 pub fn lowest_seed_location(path: &Path) -> u32 {
-    read_to_string(path)
-        .unwrap()
-        .lines();
+    let file_data = read_to_string(path).unwrap();
+    let categories: Vec<_> = file_data.split("\n\n").collect();
+    let seed_numbers = get_seed_numbers(categories[0]);
+    for c in categories {
+        eprintln!("category: {}\n", c);
+    }
     todo!()
 }
--- day5/src/part2.rs
+++ day5/src/part2.rs
@@ -2,8 +2,6 @@ use std::fs::read_to_string;
 use std::path::Path;
 
 pub fn func(path: &Path) -> u32 {
-    read_to_string(path)
-        .unwrap()
-        .lines();
+    let _ = read_to_string(path);
     todo!()
 }
