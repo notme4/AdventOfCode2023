7dff792753a02c7eb0d074ad081dac204ec92e32
notme4 <102337330+notme4@users.noreply.github.com>
2023-12-30 15:47:05 -0600

    minor refactor

--- day8/src/part1.rs
+++ day8/src/part1.rs
@@ -25,20 +25,16 @@ fn create_node(line: &str) -> Node {
     return result;
 }
 
-pub fn func(path: &Path) -> u64 {
-    let file_data = read_to_string(path).unwrap();
-    let mut lines = file_data.lines();
-    let sequence = lines.next().unwrap();
-    let _ = lines.next();
-    let nodes: Vec<Node> = lines.map(create_node).collect();
-    let mut map = HashMap::<&str, Node>::new();
-    for node in nodes {
-        map.insert(node.name, node);
-    }
+fn steps_to_traverse(
+    sequence: &str,
+    start: &str,
+    at_end: fn(&str) -> bool,
+    map: &HashMap<&str, Node>,
+) -> u64 {
     let mut i = 0;
-    let mut name = "AAA";
+    let mut name = start;
     for c in sequence.chars().cycle() {
-        if name == "ZZZ" {
+        if at_end(name) {
             break;
         }
         match c {
@@ -49,4 +45,17 @@ pub fn func(path: &Path) -> u64 {
         i += 1;
     }
     return i;
+}
+
+pub fn func(path: &Path) -> u64 {
+    let file_data = read_to_string(path).unwrap();
+    let mut lines = file_data.lines();
+    let sequence = lines.next().unwrap();
+    let _ = lines.next();
+    let nodes: Vec<Node> = lines.map(create_node).collect();
+    let mut map = HashMap::<&str, Node>::new();
+    for node in nodes {
+        map.insert(node.name, node);
+    }
+    return steps_to_traverse(sequence, "AAA", |val: &str| val == "ZZZ", &map);
 }
