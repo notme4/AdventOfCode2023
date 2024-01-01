262d9e4aeae4d59cf633bc933e165a05de8599ee
notme4 <102337330+notme4@users.noreply.github.com>
2024-01-01 00:08:33 -0600

    part 2 finished

--- day9/src/part2.rs
+++ day9/src/part2.rs
@@ -1,7 +1,41 @@
 use std::fs::read_to_string;
 use std::path::Path;
 
+fn to_vec_i64(line: &str) -> Vec<i64> {
+    line.trim()
+        .split(" ")
+        .filter_map(|val| val.parse::<i64>().ok())
+        .collect()
+}
+
+fn get_next(hist: Vec<i64>) -> i64 {
+    if hist.iter().all(|val| *val == 0) {
+        return 0;
+    }
+    let mut a = hist.iter();
+    let first = a.next().unwrap();
+    let last = hist.last().unwrap();
+    let seq = get_next(
+        hist.iter()
+            .zip(a)
+            .map(|val| val.1 - val.0)
+            .collect::<Vec<i64>>(),
+    );
+
+    eprintln!("last: {}", last);
+    return first - seq;
+}
+
+fn eprint_through<T: std::fmt::Debug>(t: T) -> T {
+    eprintln!("{:?}", t);
+    return t;
+}
+
 pub fn func(path: &Path) -> i64 {
-    read_to_string(path).unwrap();
-    todo!()
+    read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(to_vec_i64)
+        .map(get_next)
+        .sum()
 }
