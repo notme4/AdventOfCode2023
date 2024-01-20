95e8df62ba357ee209dc890f4834ad4511345641
notme4 <102337330+notme4@users.noreply.github.com>
2024-01-19 21:36:51 -0600

    finish part1

--- day15/src/part1.rs
+++ day15/src/part1.rs
@@ -2,10 +2,16 @@ use std::fs::read_to_string;
 use std::path::Path;
 
 fn hash(start_num: u8, s: &str) -> u8 {
-    s.chars().fold(start_num, |u: u8, c: char| (u + c.try_into().unwrap()).wrapping_mult(17))
+    s.chars().fold(start_num, |u: u8, c: char| {
+        u.wrapping_add(<char as TryInto<u8>>::try_into(c).expect("Ascii"))
+            .wrapping_mul(17)
+    })
 }
 
 pub fn func(path: &Path) -> u64 {
-    let file_data = read_to_string(path).unwrap();
-    todo!();
+    read_to_string(path)
+        .unwrap()
+        .split(",")
+        .map(|s| hash(0, s) as u64)
+        .sum()
 }
